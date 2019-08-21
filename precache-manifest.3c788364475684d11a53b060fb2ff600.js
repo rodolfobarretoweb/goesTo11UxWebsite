1e5cdab0810255a559d28a5227461e0dd88f6076
Rodolfo Ribeiro Barreto <rodolfobarretoweb@gmail.com>
2019-08-21 11:04:52 -0300

    Updates

---
+++ precache-manifest.3c788364475684d11a53b060fb2ff600.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeff557f0bbfb2ed1157461d6453329a",
+    "url": "/goesTo11UxWebsite/index.html"
+  },
+  {
+    "revision": "963822ae8dc926c4cbc7",
+    "url": "/goesTo11UxWebsite/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "963822ae8dc926c4cbc7",
+    "url": "/goesTo11UxWebsite/static/js/2.0983314b.chunk.js"
+  },
+  {
+    "revision": "6878020b32c660ca5857",
+    "url": "/goesTo11UxWebsite/static/js/main.e7c763d6.chunk.js"
+  },
+  {
+    "revision": "2178ab80cd872a1bf677",
+    "url": "/goesTo11UxWebsite/static/js/runtime~main.71b1f1b2.js"
+  },
+  {
+    "revision": "a5175d7a7ed3a8a1903d893fda2f56e8",
+    "url": "/goesTo11UxWebsite/static/media/home-banner-illustration.a5175d7a.svg"
+  },
+  {
+    "revision": "7f0cf085dbc0424577e5113b4d958cd6",
+    "url": "/goesTo11UxWebsite/static/media/home-content-bg.7f0cf085.svg"
+  },
+  {
+    "revision": "9fc63119c748ecfb498a8b2b507e424c",
+    "url": "/goesTo11UxWebsite/static/media/logo-symbol.9fc63119.svg"
+  },
+  {
+    "revision": "2366a999207a444a6d3d51b892b0a465",
+    "url": "/goesTo11UxWebsite/static/media/logo.2366a999.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d82101eea3f85a7487372824b02c13d.js"
+  "/goesTo11UxWebsite/precache-manifest.3c788364475684d11a53b060fb2ff600.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goesTo11UxWebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
